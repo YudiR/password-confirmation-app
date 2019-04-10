7e31bb041d1af54c5f2b324a0717bb3f5d72895a
YudiR <yudrose13@gmail.com>
2019-04-09 22:31:05 -0400

    completed authentication made model for articles

---
+++ db/migrate/20190410020647_add_a_user_id_to_posts.rb
@@ -0,0 +1,5 @@
+class AddAUserIdToPosts < ActiveRecord::Migration[5.2]
+  def change
+
+  end
+end
---
+++ db/migrate/20190410021752_add_a_user_id_to_article.rb
@@ -0,0 +1,4 @@
+class AddAUserIdToArticle < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
